299b28ce8117772eafd97bae9a8da4e81ae26d7b
Sami Ma <sami.ma3@gmail.com>
2016-02-14 03:17:40 +0000

    Create users controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  # Returns the full title if there is not one specified
+  def full_title(page_title = '')
+    base_title = "GeoHunter"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -7,6 +9,7 @@ Rails.application.routes.draw do
   root 'static_pages#index'
   get 'game' => 'games#index'
   get 'about' => 'static_pages#about'
+  get 'signup' => 'users#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
