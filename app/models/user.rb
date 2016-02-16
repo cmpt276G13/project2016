634f2645c6923204339a6221502c4d73bb0a87cb
Sami Ma <sami.ma3@gmail.com>
2016-02-16 06:22:45 +0000

    Add indexes to both username and email
    This requires that both useranames and emails are unique in the database. This also makes these two fields keys, so we can search for them using the respective fields.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
+  before_save {
+    #Ensures entries are unique in the database
+    self.username = username.downcase,
+    self.email = email.downcase
+  }
   validates :username, presence: true, length: {maximum: 50},
             uniqueness: {case_sensitive: false}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160216051334) do
+ActiveRecord::Schema.define(version: 20160216061603) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
@@ -21,5 +21,8 @@ ActiveRecord::Schema.define(version: 20160216051334) do
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
