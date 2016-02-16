fd192ae013777e68be264ccb00e99f84003f7745
Sami Ma <sami.ma3@gmail.com>
2016-02-16 06:13:17 +0000

    Create more validations for username and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
-  validates :username, presence: true, length: {maximum: 50}
-  validates :email, presence: true, length: {maximum: 255}
+  validates :username, presence: true, length: {maximum: 50},
+            uniqueness: {case_sensitive: false}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 255},
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: {case_sensitive: false}
   has_secure_password
 end
