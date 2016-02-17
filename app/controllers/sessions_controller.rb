ac7e7d4bc6baed78b4034c31edadde6247cacc0f
Sami Ma <sami.ma3@gmail.com>
2016-02-17 04:51:54 +0000

    Add a remember me checkbox

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:session][:username].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      remember user
+      # condition ? if true : if false
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user # Could redirect to the hub page instead of the profile
     else
       flash.now[:danger] = 'Invalid username/password combination'
