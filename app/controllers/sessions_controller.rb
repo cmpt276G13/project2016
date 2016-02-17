12450dda60e07bdfe3edb699f614a5ae3355f2da
Sami Ma <sami.ma3@gmail.com>
2016-02-17 02:36:15 +0000

    Create allowance for login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(username: params[:session][:username].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show page.
+      log_in user
+      redirect_to user # Could redirect to the hub page instead of the profile
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
