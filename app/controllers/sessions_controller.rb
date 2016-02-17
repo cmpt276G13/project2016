e7161cccb65ba7760e4682370e7ef04c1f316790
Sami Ma <sami.ma3@gmail.com>
2016-02-17 02:49:06 +0000

    Link some of the header links to respective pages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       log_in user
       redirect_to user # Could redirect to the hub page instead of the profile
     else
-      flash.now[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = 'Invalid username/password combination'
       render 'new'
     end
   end
