0d5c1e5d166152c37ebe69f5d6c7303356274a78
Sami Ma <sami.ma3@gmail.com>
2016-02-17 06:46:11 +0000

    Change players migration to add default values and gold field

--- db/migrate/20160217063107_create_players.rb
+++ db/migrate/20160217063107_create_players.rb
@@ -2,11 +2,12 @@ class CreatePlayers < ActiveRecord::Migration
   def change
     create_table :players do |t|
       t.string :username
-      t.integer :health
-      t.integer :strength
-      t.integer :defense
-      t.integer :level
-      t.integer :experience
+      t.integer :health, default: 50
+      t.integer :strength, default: 10
+      t.integer :defense, default: 10
+      t.integer :level, default: 1
+      t.integer :experience, default: 0
+      t.integer :gold, default: 0
 
       t.timestamps null: false
     end
