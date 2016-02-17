a243b0bb2275fad5f780c2de29fac9fac063738c
Sami Ma <sami.ma3@gmail.com>
2016-02-17 03:37:45 +0000

    Create remember_digest field for storing cookies

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160216061603) do
+ActiveRecord::Schema.define(version: 20160217033511) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160216061603) do
     t.boolean  "admin",           default: false
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
+    t.string   "remember_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
