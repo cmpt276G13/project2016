8d33b76f38a288102f7a451e07afa2d7bbaebcbb
Sami Ma <sami.ma3@gmail.com>
2016-02-17 03:31:38 +0000

    Create logout and lots of test code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the GeoHunter!"
       redirect_to @user # Change this if we want to redirect to a tutorial
     else
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,7 +4,7 @@ class UserTest < ActiveSupport::TestCase
   
   def setup
     @user = User.new(
-      username: "ExampleUser",
+      username: "exampleuser",
       email: "user@example.com",
       password: "foobar",
       password_confirmation: "foobar"
