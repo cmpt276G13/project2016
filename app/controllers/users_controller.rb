525e46c754de6233bff75d5ed22ca5ebf547ee6b
Sami Ma <sami.ma3@gmail.com>
2016-02-18 03:52:07 +0000

    Fix edit users logical bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,8 +20,13 @@ class UsersController < ApplicationController
   
   def edit
     @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      # Handle a successful update.
+      flash[:success] = "Profile updated"
+      redirect_to @user
     else
       render 'edit'
     end
