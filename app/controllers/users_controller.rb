1cb7ec28dd48d79f6677c93927392fd491f2a5a4
Sami Ma <sami.ma3@gmail.com>
2016-02-19 01:53:16 +0000

    Add creating player upon signup and it's respective test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      #@player = @user.create_player
+      @player = @user.create_player
       log_in @user
       flash[:success] = "Welcome to GeoHunter!"
       redirect_to @user # Change this if we want to redirect to a tutorial
--- test/models/player_test.rb
+++ test/models/player_test.rb
@@ -3,16 +3,14 @@ require 'test_helper'
 class PlayerTest < ActiveSupport::TestCase
   def setup
     @user = users(:michael)
-    # This code is not idiomatically correct.
-    @player = @user.build_player
   end
   
   test "should be valid" do
-    assert @player.valid?
+    assert @user.player.valid?
   end
 
   test "user id should be present" do
-    @player.user_id = nil
-    assert_not @player.valid?
+    @user.player.user_id = nil
+    assert_not @user.player.valid?
   end
 end
