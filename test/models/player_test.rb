c54b28bd41ab453dbe03f7029a53769346c4165b
Sami Ma <sami.ma3@gmail.com>
2016-02-19 01:20:29 +0000

    Add validation and testing for players

--- test/models/player_test.rb
+++ test/models/player_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class PlayerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:michael)
+    # This code is not idiomatically correct.
+    @player = @user.build_player
+  end
+  
+  test "should be valid" do
+    assert @player.valid?
+  end
+
+  test "user id should be present" do
+    @player.user_id = nil
+    assert_not @player.valid?
+  end
 end
