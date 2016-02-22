8916f7a435a72e60e21bc8b3fbb4fcf2dacdfe0a
Sami Ma <sami.ma3@gmail.com>
2016-02-22 05:11:16 +0000

    Fix top 10 rankings

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,8 @@
 class StaticPagesController < ApplicationController
   before_action :logged_in_user, only: :hub
-  layout "signuploginlayout", only: [:about,:hub]
   
   def index
-    @users = User.includes(:player).order("players.level DESC, players.experience DESC")
+    @users = User.order_by_points.first(10)
   end
   
   def hub
