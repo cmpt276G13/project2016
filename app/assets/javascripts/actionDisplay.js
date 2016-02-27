4128632d3ac83f0a9f5ae170ba7db620d51e5793
irrelevance <nimesh.shrestha@hotmail.com>
2016-02-27 22:19:48 +0000

    Created scrollable action list

--- app/assets/javascripts/actionDisplay.js
+++ app/assets/javascripts/actionDisplay.js
@@ -7,8 +7,12 @@ function actionDisplay(x, y, width, height, actionTexts) {
     this.background = createTextboxBackground(x, y, width, height);
     this.background.fixedToCamera = true;
     
-    //index of currently selected action (corresponds to the array of action texts)
-    this.selectedAction = 0;
+    //index of currently selected action (corresponds to the array of action texts), used to get the string of the selected action
+    this.idSelectedAction = 0;
+    
+    //id used to determine position of the selection bar
+    //this is a value between 0 and viewableObjects - 1
+    this.idActionToHighlight = 0;
     
     //object that indicates what action is currently being selected
     //the constructor for graphics takes in a position, but the position actually sets the origin for hte graphics obejct
@@ -16,8 +20,10 @@ function actionDisplay(x, y, width, height, actionTexts) {
     this.selectionDisplay = game.add.graphics(0, 0);
     this.background.addChild(this.selectionDisplay);
     
+    this.viewableObjects = 6;
+    
     //the text that will be displayed
-    this.actionTextList = new scrollableObjectList({x: 5, y: 5, cellWidth: width - 10, cellHeight: 28, objectCreationFunction: text, viewableObjects: 4}); //createActionTexts(actionTexts, this.background);
+    this.actionTextList = new scrollableObjectList({x: 5, y: 5, cellWidth: width - 10, cellHeight: 28, objectCreationFunction: text, viewableObjects: this.viewableObjects}); //createActionTexts(actionTexts, this.background);
     
     //add all the texts
     for(var i = 0; i < actionTexts.length; ++i) {
@@ -37,7 +43,7 @@ actionDisplay.prototype.highlightSelectedAction = function() {
     this.selectionDisplay.clear();
     
     //don't draw anything if there is no text
-    if(this.selectedAction >= this.actionTextList.objects.length) {
+    if(this.idActionToHighlight >= Math.min(this.actionTextList.objects.length, this.viewableObjects)) {
         
         return;
     }
@@ -46,8 +52,8 @@ actionDisplay.prototype.highlightSelectedAction = function() {
     this.selectionDisplay.lineStyle(2, 0x0000FF, 1);
     
     //now draw a new rectangle around the current selection
-    var xPosition = this.actionTextList.objects[this.selectedAction].configuration.x + this.actionTextList.configuration.x;
-    var yPosition = this.actionTextList.objects[this.selectedAction].configuration.y + this.actionTextList.configuration.y;
+    var xPosition = this.actionTextList.objects[this.idActionToHighlight].configuration.x + this.actionTextList.configuration.x;
+    var yPosition = this.actionTextList.objects[this.idActionToHighlight].configuration.y + this.actionTextList.configuration.y;
     
     this.selectionDisplay.drawRect(xPosition, yPosition, 100, 30);
 };
@@ -55,22 +61,47 @@ actionDisplay.prototype.highlightSelectedAction = function() {
 //users will be able to change their currently selected action
 actionDisplay.prototype.selectNext = function() {
     
-    this.selectedAction = (this.selectedAction + 1) % this.actionTextList.objects.length;
+    this.idActionToHighlight += 1;
+    this.idSelectedAction += 1;
+    
+    if(this.idActionToHighlight >= Math.min(this.viewableObjects, this.actionTextList.objects.length)) {
+        
+        this.idActionToHighlight = Math.min(this.viewableObjects, this.actionTextList.objects.length) - 1;
+        this.actionTextList.scrollDown();
+    }
+    
+    if(this.idSelectedAction >= this.actionTextList.objects.length) {
+        
+        this.idSelectedAction = this.actionTextList.objects.length - 1;
+    }
+    
 };
 
 actionDisplay.prototype.selectPrevious = function() {
     
-    this.selectedAction = this.selectedAction - 1 < 0 ? this.actionTextList.objects.length - 1 : this.selectedAction - 1;
+    this.idActionToHighlight -= 1;
+    this.idSelectedAction -= 1;
+    
+    if(this.idActionToHighlight < 0) {
+        
+        this.idActionToHighlight = 0;
+        this.actionTextList.scrollUp();
+    }
+    
+    if(this.idSelectedAction < 0) {
+        
+        this.idSelectedAction = 0;
+    }
 };
 
 actionDisplay.prototype.getSelectedActionString = function() {
     
-    if(this.selectedAction >= this.actionTextList.objects.length) {
+    if(this.idSelectedAction >= this.actionTextList.objects.length) {
         
         return "";
     }
     
-    return this.actionTextList.objects[this.selectedAction].text.text;
+    return this.actionTextList.objects[this.idSelectedAction].text.text;
 };
 
 //onKeyDown listener for action Display objects
