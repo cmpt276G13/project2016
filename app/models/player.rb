0de42c42346c30055a32c6a0cf314a8560f13725
Sami Ma <sami.ma3@gmail.com>
2016-02-29 01:37:07 +0000

    Add items array to player model

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,5 @@
 class Player < ActiveRecord::Base
   belongs_to :user
   validates :user_id, presence: true
+  serialize :items # Use eg. @player.items = ['item', 'x']. Don't forget to use @player.save
 end
