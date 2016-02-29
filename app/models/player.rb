63157112de8035705a36cfbab5d1fde16e2c1e9a
Sami Ma <sami.ma3@gmail.com>
2016-02-29 23:10:03 +0000

    Change the serialize to allow appending

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,7 @@
 class Player < ActiveRecord::Base
   belongs_to :user
   validates :user_id, presence: true
-  # Use eg. @player.items = ['item', 'x']. Don't forget to use @player.save
-  serialize [:items, :ongoing_quests, :completed_quests]
+  # Use eg. @player.items = ['name', 'id']
+  # Or append the array @player.ongoing_quests << 2. Don't forget to use @player.save
+  serialize [:items, :ongoing_quests, :completed_quests], Array
 end
