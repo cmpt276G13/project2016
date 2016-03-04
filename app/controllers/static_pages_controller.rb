2216ee2b8e6fcab9e08eace86644bb9615950c4d
ralphgabrinao <ralph.mari@yahoo.com>
2016-03-04 22:56:29 +0000

    made lat and lng variables in static_pages_controller.rb

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,5 +6,23 @@ class StaticPagesController < ApplicationController
   
   def hub
     @user_location = get_user_location
+    
+    @vancouver_lat = 49.2827
+    @vancouver_lng = -123.1207
+    
+    @paris_lat = 48.8567
+    @paris_lng = 2.3508
+    
+    @tokyo_lat = 35.6833
+    @tokyo_lng = 139.6833
+    
+    @sydney_lat = -33.8650
+    @sydney_lng = 151.2094
+    
+    @cairo_lat = 30.0500
+    @cairo_lng = 31.2333
+    
+    @brasilia_lat = -15.7939
+    @brasilia_lng = -47.8828
   end
 end
