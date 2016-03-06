4944ee1378433bf3da391fe59687ac0228168100
Sami Ma <sami.ma3@gmail.com>
2016-03-06 06:12:30 +0000

    Add preliminary tests for items show & index

--- test/integration/items_index_test.rb
+++ test/integration/items_index_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class ItemsIndexTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @admin = users(:michael)
+    file = File.read("app/assets/items/items.json")
+    @items = JSON.parse(file)
+  end
+  
+  test "index" do
+    log_in_as(@admin)
+    get items_path
+    assert_template 'items/index'
+    @items.each do |name, array|
+      assert_select 'a[href=?]', item_path(name), text: name
+    end
+  end
 end
