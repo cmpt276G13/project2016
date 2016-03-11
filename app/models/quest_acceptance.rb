512f3a2c2ecd28d52784deff48c3025bc682d392
Sami Ma <sami.ma3@gmail.com>
2016-03-11 01:01:35 +0000

    Finish all fields required for game page
    Even though I have added all of the fields required, I still haven't finished the turning in of quests.

--- app/models/quest_acceptance.rb
+++ app/models/quest_acceptance.rb
@@ -1,6 +1,9 @@
 class QuestAcceptance < ActiveRecord::Base
   belongs_to :player
   belongs_to :quest
+  # progress hash should have the name and the amount killed/gathered/etc.
+  # Ex. { orc: 2, some_monster: 1 }
+  serialize :progress, Hash
   after_initialize :init
   
   def init
