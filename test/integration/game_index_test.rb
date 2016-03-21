552276a486410c0a795978edc96862f7bda1fe50
Sami Ma <sami.ma3@gmail.com>
2016-03-21 17:15:13 +0000

    Test game page redirecting

--- test/integration/game_index_test.rb
+++ test/integration/game_index_test.rb
@@ -1,23 +1,4 @@
 require 'test_helper'
 
 class GameIndexTest < ActionDispatch::IntegrationTest
-  def setup
-    @user = users(:michael)
-    @other_user = users(:archer)
-  end
-  
-  test "should redirect game when not accesed from hub" do
-    log_in_as(@user)
-    get root_path
-    get game_path
-    assert_redirected_to hub_path
-  end
-  
-  # Test is not working right now.
-  # test "should go to game when accesed from hub" do
-  #   log_in_as(@user)
-  #   request.env['HTTP_REFERER'] = hub_url
-  #   get game_path
-  #   assert_redirected_to game_path
-  # end
 end
