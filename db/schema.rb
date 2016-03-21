0579f5679394ac37f5657437758a189be78d5735
Sami Ma <sami.ma3@gmail.com>
2016-03-21 01:14:13 +0000

    Add skills and mana fields to player

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160310224300) do
+ActiveRecord::Schema.define(version: 20160321010847) do
 
   create_table "places", force: :cascade do |t|
     t.integer  "user_id"
@@ -40,6 +40,11 @@ ActiveRecord::Schema.define(version: 20160310224300) do
     t.integer  "experience_to_next_level", default: 10
     t.integer  "deaths",                   default: 0
     t.text     "items",                    default: "--- {}\n"
+    t.text     "skills",                   default: "--- {}\n"
+    t.integer  "mana",                     default: 25
+    t.integer  "max_mana",                 default: 25
+    t.integer  "magic_power",              default: 10
+    t.integer  "magic_defense",            default: 10
   end
 
   add_index "players", ["user_id"], name: "index_players_on_user_id"
