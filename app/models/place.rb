593d23bc01c70e525909ac75c9bd4b3c9bb184e7
ralphgabrinao <ralph.mari@yahoo.com>
2016-03-21 00:54:12 +0000

    added validations to places

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,7 @@
 class Place < ActiveRecord::Base
     belongs_to :user
-
+    validates :address, presence: true
+    validates :title, presence: true
     validates :user_id, presence: true
     geocoded_by :address
     after_validation :geocode
