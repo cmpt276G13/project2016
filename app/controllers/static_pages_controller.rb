5063ff094d2185de52363fc10befedd1beb96976
kelvin94 <kelvin94@live.cn>
2016-03-23 02:38:55 +0000

    finish basic format

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
-  before_action :logged_in_user, only: :hub
+  before_action :logged_in_user, only: [:hub, :tutorial_page]
   def index
     # Users are now by default ordered by points. Check user.rb.
     @users = User.first(10)
@@ -8,5 +8,7 @@ class StaticPagesController < ApplicationController
   def hub
     @user_location = get_user_location
     init_markers
+  end
+  def tutorial
   end
 end
