d92a1df4f7f1e61a80d5bd80dd81e30d9c2c1f0d
Sami Ma <sami.ma3@gmail.com>
2016-03-30 06:47:40 +0000

    Finish skills controller methods
    I haven't created the views to go along with them, yet. I'm also thinking of creating a header for the shop to switch to the skills index.

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,2 +1,19 @@
 class SkillsController < ApplicationController
+  attr_accessor :file
+  before_action :get_skills_file
+  before_action :logged_in_user
+  
+  def index
+    @skills = JSON.parse self.file
+  end
+  
+  def show
+    @skill = JSON.parse(self.file)[params[:name]]
+  end
+  
+  private
+  
+    def get_skills_file
+      self.file = File.read("app/assets/skills/skills.json")
+    end
 end
