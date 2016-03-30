522568852a925ba6e359c2469f4a5e1d62568a7f
kelvin94 <kelvin94@live.cn>
2016-03-30 02:47:45 +0000

    changes size of the gifs

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,6 +4,7 @@ class StaticPagesController < ApplicationController
     # Users are now by default ordered by points. Check user.rb.
     @users = User.first(10)
   end
+  
   def gameinfo
   end
 end
