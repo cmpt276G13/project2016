6146c038d65e0cb3a0bae9f0565babcd94e165fc
Sami Ma <sami.ma3@gmail.com>
2016-04-01 08:19:06 +0000

    Format numbers for large prices

--- test/controllers/skills_controller_test.rb
+++ test/controllers/skills_controller_test.rb
@@ -5,7 +5,8 @@ class SkillsControllerTest < ActionController::TestCase
     @user = users(:michael)
     file = File.read("app/assets/skills/skills.json")
     @skills = JSON.parse file
-    @skill = @skills[@skills.keys[3]]
+    @ID = 3
+    @skill = @skills[@skills.keys[@ID]]
   end
   
   test "should redirect index when not logged in" do
@@ -15,7 +16,7 @@ class SkillsControllerTest < ActionController::TestCase
   end
   
   test "should redirect show when not logged in" do
-    get :show, name: @skills.keys[3]
+    get :show, name: @skills.keys[@ID]
     assert_not flash.empty?
     assert_redirected_to login_url
   end
@@ -23,7 +24,14 @@ class SkillsControllerTest < ActionController::TestCase
   test "back button should direct to referrer" do
     log_in_as(@user)
     request.env["HTTP_REFERER"] = skills_url
-    get :show, name: @skills.keys[3]
+    get :show, name: @skills.keys[@ID]
+    assert_select 'a[href=?]', skills_url, text: "Back"
+  end
+  
+  test "After buying item, back button should go to items index" do
+    log_in_as(@user)
+    request.env["HTTP_REFERER"] = skill_url(@skills.keys[@ID])
+    get :show, name: @skills.keys[@ID]
     assert_select 'a[href=?]', skills_url, text: "Back"
   end
 end
